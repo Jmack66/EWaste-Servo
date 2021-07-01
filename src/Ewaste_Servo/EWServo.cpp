8da04d4222f65ab260fdc07f673fe8e034f6f90b
mack.jonah@gmail.com <mack.jonah@gmail.com>
2021-07-01 15:52:54 -0700

    improved and moved

---
+++ src/Ewaste_Servo/EWServo.cpp
@@ -0,0 +1,101 @@
+#include "EWServo.h"
+
+
+EWServo::EWServo(int in1, int in2, int out) {
+  input1 = in1;
+  input2 = in2;
+  feedback = out;
+  pinMode(input1, OUTPUT);
+  pinMode(input2, OUTPUT);
+  pinMode(feedback, INPUT);
+}
+
+void EWServo::setLims(int min, int max){
+	min_servo = min;
+	max_servo = max;
+}
+
+void EWServo::setSpeed(int speed){
+	drive = speed;
+}
+
+void EWServo::setTolerance(int tol){
+	tolerance = tol;
+}
+
+
+void EWServo::setFailure(int failure){
+	failure = 50000;
+}
+
+void EWServo::setAll(int min, int max,int tol,int failure,int speed){
+setLims(min,max);
+setSpeed(tol);
+setTolerance(failure);
+setFailure(speed);
+}
+
+void EWServo::calibrate() {
+
+}
+
+bool EWServo::write(int pos){
+	pos = map(pos,min_servo,max_servo,min_analog,max_analog);
+	if(checkBounds(pos)){return false;}
+	int count = 0;
+	while(pot_pos < pos + tolerance && pot_pos > pos - tolerance){
+		count++;
+		pot_pos = analogRead(feedback);
+		if(pot_pos < pos){
+				servoDrivePos(count);
+		}else if(pot_pos > pos){
+				servoDriveNeg(count);
+		}
+		if(count > failure){
+			write(200);
+			//Temporary fix to prevent servo from getting stuck 
+		}
+	}
+	driveStop();
+	return true;
+}
+
+bool EWServo::checkBounds(int pos){
+	if(pos <= min_servo + tolerance || pos >= max_servo - tolerance){
+		driveStop();
+		Serial.println("ERROR: Position request out of bounds");
+		return false;
+	}else{
+		return true;
+	}
+}
+
+
+void EWServo::stop(){
+driveStop();
+}
+
+void EWServo::servoDrivePos(int count){
+	drivePos();
+	if(count % 4 == 0){driveStop();}
+}
+
+void EWServo::servoDriveNeg(int count){
+	driveNeg();
+	if(count % 4 == 0){driveStop();}
+}
+
+void EWServo::drivePos() {
+  digitalWrite(input2, LOW);
+  analogWrite(input1, drive);
+}
+
+void EWServo::driveStop(){
+    digitalWrite(input1, LOW);
+  analogWrite(input2, 0);
+ }
+
+void EWServo::driveNeg() {
+  digitalWrite(input1, LOW);
+  analogWrite(input2, drive);
+}
---
+++ src/Ewaste_Servo/EWServo.h
@@ -0,0 +1,35 @@
+#include <Wire.h>
+//for now wire is included to use serial print for debug-- this can be removed so there is no library dependencies other than the arduino ones 
+class EWServo{
+  public:
+    EWServo(int in1, int in2, int out);
+    void calibrate();
+    bool write(int pos);
+    void stop();  
+    void setLims(int min, int max);
+    void setTolerance(int tol);
+    void setFailure(int failure);
+    void setSpeed(int speed);
+    void setAll(int min = 0, int max = 180,int tol = 15,int failure = 5000,int speed = 300);
+    bool checkBounds(int pos);
+    //until calibrate is working these are hardcoded
+    int min_analog = 0;
+    int max_analog = 400;
+  private:
+    int input1;
+    int input2;
+    int feedback;
+    int pot_pos;
+    int servo_pos;
+    int min_servo; 
+    int max_servo;
+    int tolerance;
+    int drive;
+    int failure;
+    void drivePos();
+    void driveNeg();
+    void driveStop();
+    void servoDrivePos(int count);
+    void servoDriveNeg(int count);
+ 
+ };
